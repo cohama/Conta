b337c59d80650c6f92e633a7e3ccaa71a5ab6b2f
cohama <hirotaka.hamada.1024@gmail.com>
2011-01-15 03:04:32 +0900

    格子点表示において、不等間隔格子に対応した

--- Visualize/GridPointField.cs
+++ Visualize/GridPointField.cs
@@ -23,16 +23,17 @@ namespace Visualization
 			}
 			Graphics g = Graphics.FromImage( bmp );
 			Pen p = new Pen( Color.Black );
-			int origin = Visualize.BmpMargin - 1;
-			double unitX = (double)(bmp.Width - 2*Visualize.BmpMargin - 1) / (data.Columns - 1);
-			double unitY = (double)(bmp.Height- 2*Visualize.BmpMargin - 1) / (data.Rows - 1);
+			int originX = Visualize.BmpMargin - 1;
+			int originY = bmp.Height - Visualize.BmpMargin;
+			double unitX = (double)(bmp.Width - 2*Visualize.BmpMargin - 1) / (data.GetX( data.Columns-1 ) - data.GetX( 0 ) );
+			double unitY = (double)(bmp.Height- 2*Visualize.BmpMargin - 1) / (data.GetY( data.Rows-1 ) - data.GetY( 0 ) );
 			for( int i=0; i<data.Columns; i++ )
 			{
 				for( int j=0; j<data.Rows; j++ )
 				{
-					int x = (int)(i*unitX + 0.5);
-					int y = (int)(j*unitY + 0.5);
-					g.DrawRectangle( p, origin + x, origin + y, 2, 2 );
+					int x = (int)(data.GetX( i )*unitX + 0.5);
+					int y = (int)(data.GetY( j )*unitY + 0.5);
+					g.DrawRectangle( p, originX + x, originY - y, 2, 2 );
 				}
 			}
 		}
