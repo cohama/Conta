1c5fdc91e2ca2e11d7c851c866b2249518bc6191
cohama <hirotaka.hamada.1024@gmail.com>
2011-01-23 00:49:42 +0900

    格子点の表示位置が少しずれていたのを修正

--- Visualize/GridPointField.cs
+++ Visualize/GridPointField.cs
@@ -23,9 +23,9 @@ namespace Visualization
 			}
 			Graphics g = Graphics.FromImage( canvas.Bitmap );
 			Pen p = new Pen( Color.Black );
-			int originX = Visualize.BmpMargin - 1;
-			int originY = canvas.Bitmap.Height - Visualize.BmpMargin;
-			double unitX = (double)(canvas.Bitmap.Width - 2*Visualize.BmpMargin - 1) / (data.GetX( data.Columns-1 ) - data.GetX( 0 ));
+			int originX = Visualize.BmpMargin;
+			int originY = canvas.Bitmap.Height - Visualize.BmpMargin - 1;
+			double unitX = (double)(canvas.Bitmap.Width - 2*Visualize.BmpMargin -1) / (data.GetX( data.Columns-1 ) - data.GetX( 0 ));
 			double unitY = (double)(canvas.Bitmap.Height- 2*Visualize.BmpMargin - 1) / (data.GetY( data.Rows-1 ) - data.GetY( 0 ));
 			for( int i=0; i<data.Columns; i++ )
 			{
@@ -33,7 +33,7 @@ namespace Visualization
 				{
 					int x = (int)(data.GetX( i )*unitX + 0.5);
 					int y = (int)(data.GetY( j )*unitY + 0.5);
-					g.DrawRectangle( p, originX + x, originY - y, 2, 2 );
+					g.DrawRectangle( p, originX + x - 1, originY - y -1, 2, 2 );
 				}
 			}
 		}
